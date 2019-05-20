cac7cd181a9c0f66fa63bdc589eeafa6496051a7
Conrad Eggan <conrade@redcatmfg.com>
2019-05-20 09:29:33 -0500

    Added test module

---
+++ TestSendReqToProxy.py
@@ -0,0 +1,34 @@
+import requests
+from requests.compat import urljoin
+
+
+def api_get(base_url, api, token, parameters=None):
+    api_endpoint = 'http://' + base_url + '/' + api
+    headers = {'Content-Type': 'application/json',
+               }
+
+    r = requests.get(url=api_endpoint, headers=headers, params=parameters)
+
+
+def api_post(base_url, api, parameters):
+    api_endpoint = urljoin(base_url, api)
+    headers = {'Content-Type': 'application/json',
+               }
+    post = requests.post(url=api_endpoint, headers=headers, json=parameters)
+    return post
+
+
+base_url = 'http://192.168.0.10:8080'
+api = 'iot-stg-redirect/data/add/'
+
+data = {'tag': 'test_integer',
+        'value': '1',
+        }
+
+r = api_post(base_url, api, data)
+print('Status:', r.status_code)
+print('Reason:', r.reason)
+print('Response text:', r.text)
+print('Requested URL:', r.url)
+
+
